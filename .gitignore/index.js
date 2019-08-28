1858ee77416ab3cb08bc8eb01e2bc2ed9ef931dd
luke12343421 <41844191+luke12343421@users.noreply.github.com>
2019-08-28 15:25:48 -0400

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,79 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const GroupID1 = ('603284970362109966');
+
+const timer_delete = 9000 // 10000 = 11 S
+
+const message_no = 'Vous vous êtes trompé de chanel aller dans vérification pour avoir le rôle Joueur'
+
+const message_no_3 = 'Vous vous êtes trompé de chanel aller dans vérification pour avoir le rôle Joueur...'
+
+const message_no_2 = 'Vous vous êtes trompé de chanel aller dans vérification pour avoir le rôle Joueur..'
+
+const message_no_1 = 'Vous vous êtes trompé de chanel aller dans vérification pour avoir le rôle Joueur.'
+
+client.on("ready", () => {
+    client.user.setActivity("Ceria/DarkRP", { type: 'WATCHING' });
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name)
+
+        // List all channels
+        guild.channels.forEach((channel) => {
+            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id} `)
+        })
+    })
+});
+
+client.on('message', async message => { 
+  if(message.author.bot) return;
+  //if(message.content === "!" + " " + "clear bot"){
+  //  message.channel.bulkDelete(message | 2).then(() => { });
+//  }
+  if (message.channel.id === '614864376934957156') {
+  if(message.content === "!" + " " + "vérification") {
+    const member = message.member
+    member.addRole(GroupID1)
+    console.log(`${message.author.username}!`),
+    console.log(`${message.author.id}!`),
+    console.log(`${message.author.avatarURL}!`),
+    //console.log(client.channels.get('613440449260290049')//.send(`${message.author.username}! id = ${message.author.id}`))
+    console.log(`[Joueur]`)
+    message.channel.bulkDelete(message | 1).then(() => { });
+  };
+  if(message.content === "!" + " " + "verification") {
+    
+    const member = message.member
+    member.addRole(GroupID1)
+    console.log(`${message.author.username}!`),
+    console.log(`${message.author.id}!`),
+    console.log(`${message.author.avatarURL}!`),
+    //console.log(client.channels.get('613440449260290049')//.send(`${message.author.username}! id = ${message.author.id}`))
+    console.log(`[Joueur]`)
+    message.channel.bulkDelete(message | 1).then(() => { });
+  };        
+    }else{
+      if(message.content === "!" + " " + "verification"){
+        test(message);
+    message.reply(message_no)    .then(msg => {
+    msg.delete(timer_delete);
+  });
+      };
+          if(message.content === "!" + " " + "vérification"){
+        test(message);
+           message.reply(message_no)  .then(msg => {
+    msg.delete(timer_delete);
+  });
+    };
+    }
+
+})
+  // pour ton bot login sur serveur
+client.login("token"); 
+
+function test(message) {
+  message.channel.bulkDelete(message | 1).then(() => { });
+  console.log(`${message.author.username}!, qui a le grade il samuse avec la commands !!!!!!!!`)
+}
