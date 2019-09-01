e7bb8727d0465e42cb3d216ceae0cda9c21b548f
luke12343421 <41844191+luke12343421@users.noreply.github.com>
2019-09-01 18:32:39 -0400

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -39,8 +39,8 @@ client.on('message', async message => {
     if (message.channel.id === process.env.ADMIN_COMMAND) {
         if(message.content === prefix + " " + "Admin") {
              message.channel.bulkDelete(message | 1).then(() => { });
-            if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply("**:x: Vous n'avez pas la permission dls**").catch(console.error).then(msg => {
-    msg.delete(timer_Admin);;
+            if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply("**:x: Vous n'avez pas la permission dls**").then(msg => {
+    msg.delete(timer_Admin).catch(console.error);;
          client.channels.get('607229829242290183').send(process.env.MESSAGE)
         })
     }else{
