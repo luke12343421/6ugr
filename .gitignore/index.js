dbf7f29a29879a3f22b5e436dde44555bfa2b0ce
luke12343421 <41844191+luke12343421@users.noreply.github.com>
2019-09-04 17:54:56 -0400

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -58,7 +58,7 @@ function test_1(arg) {
 }
 client.on("ready", () => {
     client.user.setActivity ("Ceria/DarkRP", { type: 'WATCHING' }).then(msg => {
-
+    setTimeout(test_1, 4000, 'funky');
     })
     console.log("Servers:")
     client.guilds.forEach((guild) => {
