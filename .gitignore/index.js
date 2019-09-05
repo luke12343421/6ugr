02cf59132501fd2ff5a61cef88c5acb2c0ee3393
luke12343421 <41844191+luke12343421@users.noreply.github.com>
2019-09-05 16:25:02 -0400

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -25,37 +25,37 @@ const message_no_2 = 'Vous vous êtes trompé de chanel aller dans vérification
 const message_no_1 = 'Vous vous êtes trompé de chanel aller dans vérification pour avoir le rôle Joueur.'
 
 function test_4(arg){
-	if(true_or_false == false) {
+	if(true_or_false = false) {
   setTimeout(test_1, timer_event, 'funky');
     client.user.setActivity ("Ceria/DarkRP", { type: 'WATCHING' });
-	}elseif(true_or_false == true);{
+	}else{
 	client.user.setActivity (`live qui est ${live}`, { type: 'STREAMING'})
 		  setTimeout(test_1, timer_event, 'funky');
 	}
 }
 function test_3(arg){
-	if(true_or_false == false) {
+	if(true_or_false = false) {
    setTimeout(test_4, timer_event, 'funky');
      client.user.setActivity ("Ceria/DarkRP...", { type: 'WATCHING' });
-	}elseif(true_or_false == true);{
+	}else{
 	client.user.setActivity (`live qui est ${live}.`, { type: 'STREAMING'})
 		   setTimeout(test_4, timer_event, 'funky');
 	}
 }
 function test_2(arg){
-	if(true_or_false == false) {
+	if(true_or_false = false) {
       setTimeout(test_3, timer_event, 'funky');
         client.user.setActivity ("Ceria/DarkRP..", { type: 'WATCHING' });
-	}elseif(true_or_false == true);{
+	}else{
 	client.user.setActivity (`live qui est ${live}..`, { type: 'STREAMING'})
 		     setTimeout(test_3, timer_event, 'funky');
 	}
 }
 function test_1(arg) {
-	if(true_or_false == false) {
+	if(true_or_false = false) {
   setTimeout(test_2, timer_event, 'funky');
   client.user.setActivity ("Ceria/DarkRP.", { type: 'WATCHING' });
-	}elseif(true_or_false == true);{
+	}else{
 	client.user.setActivity (`live qui est ${live}...`, { type: 'STREAMING'})
 		  setTimeout(test_2, timer_event, 'funky');
 	}
